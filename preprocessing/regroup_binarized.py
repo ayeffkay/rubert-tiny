1e2122b73439fd49babc3d6d593c257c2f7be6ee
Alina Kolesnikova <ayeffkay@IPAVLOV.MIPT.RU>
2021-11-20 20:28:35 +0300

    fix: filter sequence with zero probs, change regrouping

--- preprocessing/regroup_binarized.py
+++ preprocessing/regroup_binarized.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser
 import pickle
 import glob
 from pathlib import Path
+import shutil
 
 
 def main():
@@ -11,14 +12,17 @@ def main():
     args, _ = parser.parse_known_args()
     
     data = []
-    for file in glob.glob(f'{args.binarized_folder}/*'):
+    old_files = sorted(glob.glob(f'{args.binarized_folder}/*'))
+    for file in old_files:
         with open(file, 'rb') as f:
             shard = pickle.load(f)
             data.extend(shard)
     samples_per_shard = len(data) // args.n_shards
     for i in range(args.n_shards):
+        shutil.rmtree(old_files[i])
         with open(Path(args.binarized_folder)/f'shard_{i}.pickle', 'wb') as f:
             pickle.dump(data[i * samples_per_shard: (i + 1) * samples_per_shard], f)
+    
 
 if __name__ == '__main__':
     main()
