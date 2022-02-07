982dbb1d3d0ffe1bd260021fcb4e1d2c777a660f
ayeffkay <chatton1@mail.ru>
2022-02-07 08:56:20 +0300

    chore: change args in bash running script

--- distil-finetuned-en/scripts/sst2/train_teacher.sh
+++ distil-finetuned-en/scripts/sst2/train_teacher.sh
@@ -7,13 +7,13 @@ python train.py --glue_dataset sst2 \
                 --gradient_accumulation_steps 4 \
                 --log_after_n_steps 20 \
                 --lr 5e-05 \
-                --gpu_id ${g} \
-                --alpha_task ${a} \
+                --gpu_id $1 \
+                --alpha_task $2 \
                 --seed 42 \
-                --dumps_dir "sst2/${d}" \
+                --dumps_dir "sst2/$3" \
                 --wandb_config scripts/sst2/wandb_config.yaml \
-                --run_id ${d} \
-                train_single --model_name ${m} \
+                --run_id $3 \
+                train_single --model_name $4 \
                 --lr_drop_patience 2 \
                 --lr_drop_div 1.5 \
                 --valid_patience 20 \
