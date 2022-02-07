56276e8c65b2be5fdd38ad0c291e47cbf3d8211b
ayeffkay <chatton1@mail.ru>
2022-02-07 08:50:41 +0300

    chore: training configs added

---
+++ distil-finetuned-en/scripts/sst2/distil_ce_kl_mse.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+python train.py --glue_dataset sst2 \
+                --tokenizer_name bert-base-uncased \
+                --tokenizer_params scripts/sst2/tokenizer.yaml \
+                --batch_size 32 \
+                --gradient_accumulation_steps 4 \
+                --lr 5e-04 \
+                --log_after_n_steps 10 \
+                --gpu_id $1 \
+                --seed 42 \
+                --dumps_dir "sst2/$2" \
+                --wandb_config scripts/sst2/wandb_config.yaml \
+                --run_id $2 \
+                --alpha_task $3 \
+                distil_teacher --n_epochs 4 --warmup_prop 0.0001 \
+                --teacher_name bert-base-uncased \
+                --teacher_weights sst2/teacher_weights.pth \
+                --student_name distilbert-tiny-uncased \
+                --student_weights sst2/student_init_weights.pth \
+                --alpha_kl $4 --temperature 5.0 --alpha_mse $5 \
+                --project_to teacher --projection_strategy average_by_layers
--- distil-finetuned-en/scripts/sst2/train_teacher.sh
+++ distil-finetuned-en/scripts/sst2/train_teacher.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-pkill -f train.py
-
 python train.py --glue_dataset sst2 \
                 --tokenizer_name bert-base-uncased \
-                --tokenizer_params scripts/tokenizer.yaml \
+                --tokenizer_params scripts/sst2/tokenizer.yaml \
                 --batch_size 128 \
                 --gradient_accumulation_steps 4 \
                 --log_after_n_steps 20 \
                 --lr 5e-05 \
-                --gpu_id 0 \
-                --alpha_task 1 \
+                --gpu_id ${g} \
+                --alpha_task ${a} \
                 --seed 42 \
-                --dumps_dir distilbert_tiny_uncased_raw_sst2 \
-                --wandb_config scripts/wandb_config.yaml \
-                --run_id distilbert_tiny_uncased_raw_sst2 \
-                train_single --model_name distilbert-tiny-uncased \
+                --dumps_dir "sst2/${d}" \
+                --wandb_config scripts/sst2/wandb_config.yaml \
+                --run_id ${d} \
+                train_single --model_name ${m} \
                 --lr_drop_patience 2 \
                 --lr_drop_div 1.5 \
                 --valid_patience 20 \
